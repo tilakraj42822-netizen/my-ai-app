344c8720d20aea694bf80ef99f035280aa55152b
tilakraj42822-netizen <tilakraj42822@gmail.com>
2026-06-15 20:12:09 +0530

    chore: remove Android/Capacitor configuration
    Delete the Android platform directory and related Capacitor dependencies as the project transitions away from native mobile support.

--- android/app/src/main/java/com/memeverse/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.memeverse.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.memeverse.app',
-  appName: 'MemeVerse',
-  webDir: 'dist'
-};
-
-export default config;
